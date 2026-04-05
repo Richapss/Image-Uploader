aacc92a0b3b3ec9e6722ea24d90db90d72c1ba12
richapps <richadosfaldo21@gmail.com>
2026-04-06 00:57:56 +0700

    fix: add GET /docs route and docs link on homepage

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get('/docs', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/docs.html'));
+});
 app.use('/', uploadRoutes);
 app.use('/', donateRoutes);
 module.exports = app;
